82773386fde1168cb7d361595f751ea2a5a6223f
ajrvs <rxaykz1@gmail.com>
2024-03-05 10:44:29 +0530

    Updated Delete functionality

--- SimpleBlog/blog/urls.py
+++ SimpleBlog/blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("all-posts", views.all_posts, name="all_posts"),
-    path("blog/<str:title>", views.blog, name="blog")
+    path("blog", views.all_posts, name="all_posts"),
+    path("blog/<str:title>", views.blog, name="blog"),
+    path("delete/<str:title>", views.delete, name="delete"),
 ]
--- SimpleBlog/blog/views.py
+++ SimpleBlog/blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import BlogPost
 
 # Create your views here.
@@ -17,3 +17,13 @@ def blog(request, title):
     post = get_object_or_404(BlogPost, title=title)
     context = {"post":post}
     return render(request, "blog.html", context)
+
+def delete(request, title):
+    post = get_object_or_404(BlogPost, title=title)
+    context = {"post":post}
+    if request.method == "POST":
+        post.delete()
+        messages.success(request, 'Blog post deleted successfully.')
+        return redirect('home')
+    return render(request, "delete.html", context)
+    # return render(request, "home.html", context)
